83b3beff3eb38fdb85f3b4b1e8bd1def2acb47eb
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-06-02 21:30:22 +0200

    create project

---
+++ src/main/java/com/romanstetsiuk/spring_beginner_to_guru/SpringBeginnerToGuruApplication.java
@@ -0,0 +1,13 @@
+package com.romanstetsiuk.spring_beginner_to_guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeginnerToGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeginnerToGuruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/romanstetsiuk/spring_beginner_to_guru/SpringBeginnerToGuruApplicationTests.java
@@ -0,0 +1,16 @@
+package com.romanstetsiuk.spring_beginner_to_guru;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBeginnerToGuruApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
