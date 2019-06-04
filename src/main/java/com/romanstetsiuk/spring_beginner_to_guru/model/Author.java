3eaa336fbc93d86abcbb38036b03452df4e2533a
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-06-04 21:41:29 +0200

    Create Entity by Author & Books

--- src/main/java/com/romanstetsiuk/spring_beginner_to_guru/model/Author.java
+++ src/main/java/com/romanstetsiuk/spring_beginner_to_guru/model/Author.java
@@ -1,13 +1,19 @@
 package com.romanstetsiuk.spring_beginner_to_guru.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firsName;
     private String lastName;
 
+    @ManyToMany
     private Set<Book> books = new HashSet<>();
 
     public Author() {
@@ -46,5 +52,13 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
--- src/main/java/com/romanstetsiuk/spring_beginner_to_guru/model/Book.java
+++ src/main/java/com/romanstetsiuk/spring_beginner_to_guru/model/Book.java
@@ -1,14 +1,20 @@
 package com.romanstetsiuk.spring_beginner_to_guru.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
+    @ManyToMany
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
@@ -57,5 +63,13 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
